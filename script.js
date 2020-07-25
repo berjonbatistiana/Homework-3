0e0aff6545d6e6d67ed4dd8abe7e4f4337f9a6e7
Bermond Jon Batistiana <berjondev@gmail.com>
2020-07-25 11:09:40 -0700

    Fixed conditions for length checking

--- script.js
+++ script.js
@@ -15,8 +15,15 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
+  // Initialize required variables
   var password = "";
-  var isValid = false;
+  var isValid = true;
+
+  var passwordLength;
+  var hasLowerCase;
+  var hasUpperCase;
+  var hasNumeric;
+  var hasSpecial;
 
   var numericCharSelector = '0987654321';
   var lowerCaseSelector = 'abcdefghijklmnopqrstuvwxyz';
@@ -25,50 +32,45 @@ function generatePassword() {
 
   var selectorArray = [];
 
-  while (!isValid) {
 
-    var passwordLength = parseInt(prompt("How long is your password? Enter a number between 8 to 128."));
-    var hasLowerCase = confirm("Does it require lower case characters?");
-    var hasUpperCase = confirm("Does it require upper case characters?");
-    var hasNumeric = confirm("Does it require numeric characters?");
-    var hasSpecial = confirm("Does it require special characters?");
+  passwordLength = parseInt(prompt("How long is your password? Enter a number between 8 to 128."));
+  hasLowerCase = confirm("Does it require lower case characters?");
+  hasUpperCase = confirm("Does it require upper case characters?");
+  hasNumeric = confirm("Does it require numeric characters?");
+  hasSpecial = confirm("Does it require special characters?");
 
-    if (isNaN(passwordLength) && ((passwordLength > 7) && (passwordLength < 129))) {
-      alert("Please enter a number to indicate the length of the password between 8 to 128.");
-      continue;
-    }
-
-    if (!(hasSpecial || hasNumeric || hasUpperCase || hasLowerCase)) {
-      alert("Please select at least one character type.");
-      continue;
-    }
-    
-    isValid = true;
-
-    if (hasSpecial) {
-      selectorArray.push(specialCharSelector);
-    }
-
-    if (hasNumeric) {
-      selectorArray.push(numericCharSelector);
-    }
-
-    if (hasUpperCase) {
-      selectorArray.push(upperCaseSelector);
-    }
-
-    if (hasLowerCase) {
-      selectorArray.push(lowerCaseSelector);
-    }
-
-    for (var count = 0; count < passwordLength; count++) {
-      var selector = Math.floor(Math.random() * selectorArray.length);
-      var character = Math.floor(Math.random() * selectorArray[selector].length);
-      password += (selectorArray[selector][character]);
-    }
-
-    return password;
-
+  if (isNaN(passwordLength) || ((passwordLength < 8) || (passwordLength > 128))) {
+    alert("Please enter a number to indicate the length of the password between 8 to 128.");
+    isValid = false;
   }
+
+  if (!(hasSpecial || hasNumeric || hasUpperCase || hasLowerCase)) {
+    alert("Please select at least one character type.");
+    isValid = false;
+  }
+
+  if (hasSpecial) {
+    selectorArray.push(specialCharSelector);
+  }
+
+  if (hasNumeric) {
+    selectorArray.push(numericCharSelector);
+  }
+
+  if (hasUpperCase) {
+    selectorArray.push(upperCaseSelector);
+  }
+
+  if (hasLowerCase) {
+    selectorArray.push(lowerCaseSelector);
+  }
+
+  for (var count = 0; count < passwordLength; count++) {
+    var selector = Math.floor(Math.random() * selectorArray.length);
+    var character = Math.floor(Math.random() * selectorArray[selector].length);
+    password += (selectorArray[selector][character]);
+  }
+
+  return password;
 
 }
